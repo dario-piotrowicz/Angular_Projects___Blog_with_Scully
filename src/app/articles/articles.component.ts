85c837f6a28d314ff34e71e5490e8bc86e41ecb9
dario-piotrowicz <dario.piotrowicz@gmail.com>
2021-08-24 21:30:43 +0100

    show blog posts and allow routing to them

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { ScullyRoutesService } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent {
 
-  constructor() { }
+  posts$ = this.scullyService.available$.pipe(
+    map( posts => posts.filter(post => post.title) )
+  );
 
-  ngOnInit(): void {
-  }
+  constructor(private scullyService: ScullyRoutesService) { }
 
 }
